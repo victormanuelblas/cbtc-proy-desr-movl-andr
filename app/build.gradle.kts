fca2ee110e4064982dd20112fca68d48c72ec328
victormanuelblas <vmblas@gmail.com>
2025-10-30 13:03:56 -0500

    main-activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
@@ -45,4 +46,13 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.room:room-runtime:2.8.2")
+    kapt("androidx.room:room-compiler:2.8.2") // roomDB -> SQLite
+    implementation("androidx.room:room-ktx:2.8.2")
+
+    // opcional
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+
 }
--- app/src/main/java/com/rhinoseller/cbtcproyecto/MainActivity.kt
+++ app/src/main/java/com/rhinoseller/cbtcproyecto/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.rhinoseller.cbtcproyecto
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var ilClinNombres: TextInputLayout
+    lateinit var ilClinApellidos: TextInputLayout
+    lateinit var ilClinTelefono: TextInputLayout
+    lateinit var ilClinCorreo: TextInputLayout
+    lateinit var ilClinObserv: TextInputLayout
+    lateinit var etClinNombres: EditText
+    lateinit var etClinApellidos: EditText
+    lateinit var etClinTelefono: EditText
+    lateinit var etClinCorreo: EditText
+    lateinit var etClinObserv: EditText
+
+    lateinit var btnClinSiguiente: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +37,63 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Definir los controles
+        ilClinNombres = findViewById<TextInputLayout>(R.id.ilClinNombres)
+        ilClinApellidos = findViewById<TextInputLayout>(R.id.ilClinApellidos)
+        ilClinTelefono = findViewById<TextInputLayout>(R.id.etClinTelefono)
+        ilClinCorreo = findViewById<TextInputLayout>(R.id.ilClinCorreo)
+        ilClinObserv = findViewById<TextInputLayout>(R.id.ilClinObserv)
+
+        etClinNombres = findViewById<EditText>(R.id.etClinNombres)
+        etClinApellidos = findViewById<EditText>(R.id.etClinApellidos)
+        etClinTelefono = findViewById<EditText>(R.id.etClinTelefono)
+        etClinCorreo = findViewById<EditText>(R.id.etClinCorreo)
+        etClinObserv = findViewById<EditText>(R.id.etClinObserv)
+
+        btnClinSiguiente = findViewById<Button>(R.id.btnClinSiguiente)
+
+
+        btnClinSiguiente.setOnClickListener {
+            //validarFormulario()
+
+            //val intent = Intent(this, ServicioActivity::class.java)
+            //startActivity(intent)
+        }
+
     }
+
+    fun validarFormulario() {
+        val clinNombres = etClinNombres.text.toString().trim()
+        val clinApellidos = etClinApellidos.text.toString().trim()
+        val clinTelefono = etClinTelefono.text.toString().trim()
+        val clinCorreo = etClinCorreo.text.toString().trim()
+        val clinObserv = etClinObserv.text.toString().trim()
+
+        ilClinNombres.error = null
+        ilClinApellidos.error = null
+        ilClinTelefono.error = null
+        ilClinCorreo.error = null
+        ilClinObserv.error = null
+
+        var esValido = true
+
+        if (clinNombres.isEmpty()) {
+            ilClinNombres.error = "Campo obligatorio"
+            esValido = false
+        }
+        if (clinApellidos.isEmpty()) {
+            ilClinApellidos.error = "Campo obligatorio"
+            esValido = false
+        }
+        if (clinTelefono.isEmpty()) {
+            ilClinTelefono.error = "Campo obligatorio"
+            esValido = false
+        }
+        if (clinCorreo.isEmpty() || !clinCorreo.contains("@")) {
+            ilClinCorreo.error = "Correo inválido"
+            esValido = false
+        }
+    }
+
 }
